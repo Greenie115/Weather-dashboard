638435e361dc73a06548c85fc61fe2dcb52487e4
Greenie115 <dggreen545@gmail.com>
2023-11-17 19:41:55 +0000

    changed units to celcius

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userCity + '&li
         })
         
   function cityData(){
-    var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
+    var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5&units=metric'
 
       fetch(queryURL)
         .then(function(response){
