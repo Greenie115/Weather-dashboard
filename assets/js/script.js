a4fb77c810792dc8dab3c79a1c713830764f2cfa
Greenie115 <dggreen545@gmail.com>
2023-11-17 19:11:24 +0000

    fetched lon and lat from gro data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
-var userCity = 'London'
-var lat = 51.5073219
-var lon = -0.1276474
+var userCity = 'Oslo'
+var lat = 0
+var lon = -0
 var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
-var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userCity + '&limit=5&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
+var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userCity + '&limit=1&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
+
 
     fetch(geoURL)
         .then(function(response){
@@ -10,12 +11,18 @@ var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userCity + '&li
         }) 
         .then(function(data){
             console.log(data)
+            var lat = data[0].lat
+            var lon = data[0].lon
+            console.log(lat)
+            console.log(lon)
         })
 
-        fetch(queryURL)
-        .then(function(response){
-        return response.json()
-        }) 
-        .then(function(data){
-            console.log(data)
-        })
+
+
+    fetch(queryURL)
+    .then(function(response){
+    return response.json()
+    }) 
+    .then(function(data){
+        console.log(data)
+    })
