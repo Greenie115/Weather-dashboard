b4c5c39adef43a2f405d4c730084455bf5b7f0a8
Greenie115 <dggreen545@gmail.com>
2023-11-20 20:03:24 +0000

    corrected formatting for date function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 var userCity = 'york'
 var lat = 0
 var lon = -0
-var date = new Date()
-var day = date.getDate()
-var month = date.getMonth() + 1 
-var year = date.getFullYear()
-console.log(day, month, year)
+// var date = new Date()
+// var day = date.getDate()
+// var month = date.getMonth() + 1 
+// var year = date.getFullYear()
+// console.log(day, month, year)
 
 // var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
 var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userCity + '&limit=1&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
@@ -16,7 +16,6 @@ fetch(geoURL)
         return response.json()
     })
     .then(function (data) {
-        // console.log(data)
         lat = data[0].lat
         lon = data[0].lon
         var cityName = data[0].name
@@ -43,8 +42,6 @@ function cityData() {
             console.log(icon)
             var humidity = data.list[0].main.humidity
             console.log('humidity: ' + humidity + '%')
-
-
         })
 }
 
@@ -53,10 +50,14 @@ function getDate() {
     var dt = new Date();
     dt.setDate(dt.getDate());
     var stringDt = JSON.stringify(dt)
+    var day = stringDt.slice(9, 11)
+    var month = stringDt.slice(6 ,8)
+    var year = stringDt.slice(1, 5)
     console.log(stringDt.slice(1, 11));
- };
+    console.log(day, month, year)
+};
 
- getDate()
+getDate()
 
 
 
