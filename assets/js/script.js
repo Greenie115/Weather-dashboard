fc0f7177b257aa23e9454f269d13c1b6365206f3
Greenie115 <dggreen545@gmail.com>
2023-11-20 19:58:43 +0000

    working date function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,62 @@
-var userCity = 'paris'
+var userCity = 'york'
 var lat = 0
 var lon = -0
+var date = new Date()
+var day = date.getDate()
+var month = date.getMonth() + 1 
+var year = date.getFullYear()
+console.log(day, month, year)
+
 // var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
 var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userCity + '&limit=1&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
 
 
-    fetch(geoURL)
-        .then(function(response){
+fetch(geoURL)
+    .then(function (response) {
         return response.json()
-        }) 
-        .then(function(data){
-            // console.log(data)
-            lat = data[0].lat
-            lon = data[0].lon
-            var cityName = data[0].name
-            console.log(lat)
-            console.log(lon)
-            console.log(cityName)
-            cityData()
-        })
-        
-  function cityData(){
+    })
+    .then(function (data) {
+        // console.log(data)
+        lat = data[0].lat
+        lon = data[0].lon
+        var cityName = data[0].name
+        console.log(lat)
+        console.log(lon)
+        console.log(cityName)
+        cityData()
+    })
+
+function cityData() {
     var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5&units=metric'
 
-      fetch(queryURL)
-        .then(function(response){
-        return response.json()
-        }) 
-        .then(function(data){
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
             console.log(data)
             var temp = data.list[0].main.temp
-            console.log(temp)
+            console.log('temp: ' + temp)
             var wind = data.list[0].wind.speed
-            console.log(wind)
+            console.log('wind speed: ' + wind)
             var icon = data.list[0].weather[0].icon
             console.log(icon)
             var humidity = data.list[0].main.humidity
-            console.log(humidity)
-        })
-  }
+            console.log('humidity: ' + humidity + '%')
 
+
+        })
+}
+
+
+function getDate() {
+    var dt = new Date();
+    dt.setDate(dt.getDate());
+    var stringDt = JSON.stringify(dt)
+    console.log(stringDt.slice(1, 11));
+ };
+
+ getDate()
 
 
 
