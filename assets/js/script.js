7138f066cd15d0aad4105ef6c660bc198853726b
Greenie115 <dggreen545@gmail.com>
2023-11-26 18:17:14 +0000

    hardcoded li elements as the forecast is not dynamic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,11 @@ var userCity = 'york'
 var lat = 0
 var lon = 0
 var forecastDays = 5 //for creating a an array of days to display on the page
-var dailyForecast = document.getElementById('dailyForecast')
-// var date = new Date()
-// var day = date.getDate()
-// var month = date.getMonth() + 1 
-// var year = date.getFullYear()
-// console.log(day, month, year)
+var dailyForecastDiv = document.getElementById('dailyForecast')
+var weatherDayDiv = document.getElementsByClassName('weatherDay')
+var weatherDateLi = document.getElementsByClassName('date')
+var weather
 
-// var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
 var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userCity + '&limit=1&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
 
 
@@ -21,9 +18,6 @@ fetch(geoURL)
         lat = data[0].lat
         lon = data[0].lon
         var cityName = data[0].name
-        console.log(lat)
-        console.log(lon)
-        console.log(cityName)
         cityData()
     })
 
@@ -41,11 +35,7 @@ function cityData() {
             var icon = data.list[0].weather[0].icon
             var humidity = data.list[0].main.humidity
             for(i = 0; i < forecastDays; i++){
-                var weatherDayDiv = document.createElement('div')
-                var weatherDayP = document.createElement('li')
-                weatherTemp = weatherDayP.innerText = data.list[i].main.temp
-                weatherDayDiv.appendChild(weatherDayP)
-                dailyForecast.append(weatherDayDiv)
+                weatherDateLi[i].innerText = data.list[i].main.temp
             }
         })
 }
