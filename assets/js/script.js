34220a52cd185ed9a68698a6d40fb6354a8245d4
Greenie115 <dggreen545@gmail.com>
2023-12-09 16:29:10 +0000

    working data apended for forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-var userCity = 'oslo'
+var userCity = document.getElementById('userCity')
 var lat = 0
 var lon = 0
-var forecastDays = 5 //for creating a an array of days to display on the page
+var forecastDays = 5
 var dailyForecastDiv = document.getElementById('dailyForecast')
 var weatherDayDiv = document.getElementsByClassName('weatherDay')
 var weatherDateLi = document.getElementsByClassName('date')
@@ -10,40 +10,38 @@ var weatherWindSpeedLi = document.getElementsByClassName('windSpeed')
 var weatherHumidityLi = document.getElementsByClassName('humidity')
 var searchBtn = document.getElementById('searchBtn')
 
-var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userCity + '&limit=1&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
 
-// function getUserCity(){
-//     userCity = searchBtn.value
-//     console.log(userCity)
-// }
+function getUserCity(){
+    var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userCity.value + '&limit=1&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5'
+    fetch(geoURL)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            lat = data[0].lat
+            lon = data[0].lon
+            var cityName = data[0].name
+            cityData()
+        })
+}
 
-// searchBtn.addEventListener('click', getUserCity)
+searchBtn.addEventListener('click', getUserCity)
 
-fetch(geoURL)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        lat = data[0].lat
-        lon = data[0].lon
-        var cityName = data[0].name
-        cityData()
-    })
 
 function cityData() {
     var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&cnt=5&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5&units=metric'
 
+    if(!userCity){
+        weatherDateLi.innerText = ''
+        weatherWindSpeedLi.innerText = ''
+        weatherHumidityLi.innerText = ''
+    }
     fetch(queryURL)
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
             console.log(data)
-            if(!userCity){
-                weatherDateLi.innerText = ''
-                weatherWindSpeedLi.innerText = ''
-                weatherHumidityLi.innerText = ''
-            }
             for (i = 0; i < forecastDays; i++) {
                 weatherDateLi[i].innerText = 'Date: ' + getDate(i)
                 weatherWindSpeedLi[i].innerText = 'Wind Speed: ' + data.list[i].wind.speed
@@ -60,6 +58,7 @@ function cityData() {
                 };
 
             }
+            
         })
 }
 
