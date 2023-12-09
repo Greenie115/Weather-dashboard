70d324d9734b8365921bd23be3e0f68c5770a430
Greenie115 <dggreen545@gmail.com>
2023-12-09 17:05:23 +0000

    appending weather icons from API URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 var userCity = document.getElementById('userCity')
 var lat = 0
 var lon = 0
-var forecastDays = 5
+var forecastDays = 6
 var dailyForecastDiv = document.getElementById('dailyForecast')
 var weatherDayDiv = document.getElementsByClassName('weatherDay')
 var weatherDateLi = document.getElementsByClassName('date')
 var weatherTempLi = document.getElementsByClassName('temp')
 var weatherWindSpeedLi = document.getElementsByClassName('windSpeed')
 var weatherHumidityLi = document.getElementsByClassName('humidity')
+var weatherIconLi = document.getElementsByClassName('icon')
 var searchBtn = document.getElementById('searchBtn')
 var currentDayHeader = document.getElementById('currentDayHeder')
 
@@ -21,7 +22,6 @@ function getUserCity(){
         .then(function (data) {
             lat = data[0].lat
             lon = data[0].lon
-            var cityName = data[0].name
             cityData()
         })
 }
@@ -30,7 +30,7 @@ searchBtn.addEventListener('click', getUserCity)
 
 
 function cityData() {
-    var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&cnt=5&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5&units=metric'
+    var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&cnt=6&appid=b3a3fccb2b2f17b88a35bd51a8ab9db5&units=metric'
 
     if(!userCity){
         weatherDateLi.innerText = ''
@@ -44,10 +44,12 @@ function cityData() {
         .then(function (data) {
             console.log(data)
             for (i = 0; i < forecastDays; i++) {
+                var weatherIcon = data.list[i].weather[0].icon
                 weatherDateLi[i].innerText = 'Date: ' + getDate(i)
                 weatherWindSpeedLi[i].innerText = 'Wind Speed: ' + data.list[i].wind.speed
                 weatherHumidityLi[i].innerText = 'Humidity: ' + data.list[i].main.humidity + '%'
                 weatherTempLi[i].innerText = 'Temp: ' + data.list[i].main.temp + 'C'
+                weatherIconLi[i].setAttribute('src', 'https://openweathermap.org/img/wn/' + weatherIcon + '@2x.png')
                 function getDate() {
                     var dt = new Date();
                     dt.setDate(dt.getDate() + i);
